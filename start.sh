a71763af3bd4cf91589ed71c9253ad6b1289aedd
RisolMayoETH <121712363+AldiGalang@users.noreply.github.com>
2025-03-17 07:38:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-
+sudo apt install screen -y
+git clone https://github.com/AldiGalang/bot.git
+cd bot
+sudo apt install python3 && apt install python3.12-venv && python3 -m venv myenv && source myenv/bin/activate && pip install -r requirements.txt && pyton3 bot.py
