0b6e727942d6caa37290115c84e5eb7aa2075790
RisolMayoETH <121712363+AldiGalang@users.noreply.github.com>
2025-03-17 07:44:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 sudo apt update && sudo apt install screen -y
 git clone https://github.com/AldiGalang/bot.git
-screen -S gaiabot
 cd bot
-sudo apt install python3 python3.12-venv -y
-python3 -m venv myenv
-source myenv/bin/activate
-pip install -r requirements.txt
-python3 bot.py
+screen -dmS gaiabot bash -c 'sudo apt install python3 python3.12-venv -y; python3 -m venv myenv; source myenv/bin/activate; pip install -r requirements.txt; python3 bot.py'
